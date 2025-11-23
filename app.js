bc960a4b69b24bbabec3f507d0ae656cdfecc9bd
Kalcindor <kevin.alcindor@gmail.com>
2025-11-23 22:42:34 +0100

    feature: adding database configuration

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://ka_user:nlzzeLHp5QAbSEtA@clusterkal.jyeeufm.mongodb.net/?appName=ClusterKal"
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch((error) => console.log("Connexion à MongoDB échouée :", error));
 
 app.use(express.json());
 
 app.use((req, res) => {
-  res.json({ message: "Valid response" });
+  res.json({ message: "Server response" });
 });
 
 module.exports = app;
